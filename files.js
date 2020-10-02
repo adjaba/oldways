e5cd6b448d58e942d57f5b746876607701465296
Andrea Jaba <andrea@airworks.io>
2020-10-02 20:02:28 +0800

    wild try

--- files.js
+++ files.js
@@ -13,4 +13,10 @@ const files = [
   },
 ];
 
-export default files;
+const classes = {
+  ATOAH: {
+    student:
+      "ATOAH - Student Data-Marvin Young Data Sample - ACTOAH - Student Data-Marvin Young Data Sample.csv",
+    teacher: "ATOAH - Teacher Data-Marvin Youngs' Data (Teacher) .csv",
+  },
+};

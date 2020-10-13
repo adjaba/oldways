96b3dc971bfcdc7cc643d5d77075883d62486d89
Andrea Jaba <andrea@airworks.io>
2020-10-13 11:54:08 +0800

    try getting file from files.js

--- files.js
+++ files.js
@@ -1,19 +1,19 @@
 // file type can be either "student" or "teacher"
-const files = [
-  {
-    name:
-      "ATOAH - Student Data-Marvin Young Data Sample - ACTOAH - Student Data-Marvin Young Data Sample.csv",
-    class: "ATOAH",
-    type: "student",
-  },
-  {
-    name: "ATOAH - Teacher Data-Marvin Youngs' Data (Teacher) .csv",
-    class: "ATOAH",
-    type: "teacher",
-  },
-];
+// const files = [
+//   {
+//     name:
+//       "ATOAH - Student Data-Marvin Young Data Sample - ACTOAH - Student Data-Marvin Young Data Sample.csv",
+//     class: "ATOAH",
+//     type: "student",
+//   },
+//   {
+//     name: "ATOAH - Teacher Data-Marvin Youngs' Data (Teacher) .csv",
+//     class: "ATOAH",
+//     type: "teacher",
+//   },
+// ];
 
-const classes = {
+const files = {
   ATOAH: {
     student:
       "ATOAH - Student Data-Marvin Young Data Sample - ACTOAH - Student Data-Marvin Young Data Sample.csv",

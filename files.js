e664af8aeea32e8f4cf0b903841c3c04552e023d
Andrea Jaba <andrea@airworks.io>
2020-10-13 11:56:09 +0800

    files.js

--- files.js
+++ files.js
@@ -1,18 +1,3 @@
-// file type can be either "student" or "teacher"
-// const files = [
-//   {
-//     name:
-//       "ATOAH - Student Data-Marvin Young Data Sample - ACTOAH - Student Data-Marvin Young Data Sample.csv",
-//     class: "ATOAH",
-//     type: "student",
-//   },
-//   {
-//     name: "ATOAH - Teacher Data-Marvin Youngs' Data (Teacher) .csv",
-//     class: "ATOAH",
-//     type: "teacher",
-//   },
-// ];
-
 const files = {
   ATOAH: {
     student:
